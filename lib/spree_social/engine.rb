d80f0e1a59f1cdc47aecde04ede1dd5d8fdef87b
Derek Ethier <derek.ethier@gmail.com>
2012-12-08 14:50:54 -0500

    Change config to use OmniAuth builder instead of Devise

--- lib/spree_social/engine.rb
+++ lib/spree_social/engine.rb
@@ -33,15 +33,16 @@ module SpreeSocial
       if auth_method.provider == provider
         key = auth_method.api_key
         secret = auth_method.api_secret
-        puts("[Spree Social] Loading #{auth_method.provider.capitalize} as authentication source")
+        #puts("[Spree Social] Loading #{auth_method.provider.capitalize} as authentication source")
       end
     end
     self.setup_key_for(provider.to_sym, key, secret)
   end
 
   def self.setup_key_for(provider, key, secret)
-    Devise.setup do |config|
-      config.omniauth provider, key, secret
+    Rails.application.config.middleware.use OmniAuth::Builder do
+      provider provider, key, secret,
+               :scope => 'email,read_stream,read_friendlists', :auth_type => 'https'
     end
   end
 end
